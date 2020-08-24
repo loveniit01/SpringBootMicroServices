fa782b3a19257285267101023e22c6bcfed817f2
loveniit01 <loveniit01@gmail.com>
2020-08-24 13:54:30 +0530

    initial commit

---
+++ microServicesServer/src/test/java/com/pj/learn/microServicesServer/MicroServicesServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pj.learn.microServicesServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroServicesServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
